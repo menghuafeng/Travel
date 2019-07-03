7a18d6734ded418e176c7f0c9fd59a53a3d440b9
v_menghuafeng <mhf_159357>
2019-07-03 18:53:55 +0800

    城市选择页-当前城市,热门城市.所有城市布局

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,17 +2,20 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
   </div>
 </template>
 
 <script>
 import CityHeader from "./components/header";
-import CitySearch from "./components/search"
+import CitySearch from "./components/search";
+import CityList from "./components/list"
 export default {
   name : 'City',
   components: {
     CityHeader,
     CitySearch,
+    CityList,
   }
 }
 </script>
